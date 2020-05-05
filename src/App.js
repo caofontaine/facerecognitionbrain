1992ac6dbc663328ea679803abd24a0fb27c350d
Ben Cao <benjamin.cao88@gmail.com>
2020-05-05 15:26:11 -0400

    security review

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -9,10 +8,6 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import './App.css';
-
-const app = new Clarifai.App({
- apiKey: '20467f0e8b1e417c84846dea9a298118'
-});
 
 const particlesOptions = {
 	particles: {
@@ -81,9 +76,14 @@ class App extends Component {
 	
 	onButtonSubmit = () => {
 		this.setState({imageUrl: this.state.input});
-		
-		app.models
-			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+			fetch('http://localhost:3001/imageurl', {
+				method: 'post',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify({
+					input: this.state.input
+				})
+			})
+			.then(response => response.json())
 			.then(response => {
 				if (response) {
 					fetch('http://localhost:3001/image', {
