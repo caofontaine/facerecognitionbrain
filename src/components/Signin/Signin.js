7a8c60acbf8da0f13c34b1a61fd5a3ee48b23ca3
Ben Cao <benjamin.cao88@gmail.com>
2020-05-07 20:30:58 -0400

    deploy front end

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends Component {
 	}
 	
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3001/signin', {
+		fetch('https://whispering-cove-92889.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
