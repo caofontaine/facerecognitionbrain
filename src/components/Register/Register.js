f47536398a8706754e5a98e92111f338b0cd2f81
Ben Cao <benjamin.cao88@gmail.com>
2020-05-07 21:02:47 -0400

    remove prod dependencies

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends Component {
 	}
 	
 	onSubmitSignIn = () => {
-		fetch('https://whispering-cove-92889.herokuapp.com/register', {
+		fetch('http://localhost:3001/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
